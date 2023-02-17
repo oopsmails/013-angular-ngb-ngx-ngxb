5a662b639cba6c73ee99da73cc2f258227642b06
oopsmails <oopsmails@gmail.com>
2023-02-16 21:51:32 -0500

    before more on hm-datatable

--- src/app/homemade/homemade-routing.module.ts
+++ src/app/homemade/homemade-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HmDatatableComponent } from './hm-datatable/hm-datatable.component';
 import { HomeMadeDropdownSearchAsyncComponent } from './hm-dropdown-search-async/hm.dropdown.search.async.component';
 import { HomeMadeDropdownSearchComponent } from './hm-dropdown-search/hm.dropdown.search.component';
 import { HmSpinnerComponent } from './hm-spinner/hm-spinner.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'hm/hm-dropdown-search', component: HomeMadeDropdownSearchComponent },
   { path: 'hm/hm-dropdown-search-async', component: HomeMadeDropdownSearchAsyncComponent },
   { path: 'hm/hm-spinner', component: HmSpinnerComponent },
+  { path: 'hm/hm-datatable', component: HmDatatableComponent },
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
