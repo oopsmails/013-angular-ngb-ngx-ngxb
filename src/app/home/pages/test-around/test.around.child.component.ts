fc18a4320c743b56df2daa105d24fec708fff545
oopsmails <oopsmails@gmail.com>
2023-03-20 20:45:27 -0400

    test around, ng-content

--- src/app/home/pages/test-around/test.around.child.component.ts
+++ src/app/home/pages/test-around/test.around.child.component.ts
@@ -1,12 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'test-around-child',
-  template: ` <div class="list-container"></div> `,
+  template: `
+    <div class="list-container">
+      <div class="title">Child Component</div>
+      <h3>content without any selector</h3>
+      <ng-content></ng-content>
+      <hr />
+      <h3>content from selector: using id</h3>
+      <ng-content select="[titleText]"></ng-content>
+      <hr />
+      <h3>content from selector: using css class</h3>
+      <ng-content select=".desc"></ng-content>
+    </div>
+  `,
   styles: [
     `
       .list-container {
-        height: 200px;
+        height: 300px;
         overflow: auto;
       }
     `,
