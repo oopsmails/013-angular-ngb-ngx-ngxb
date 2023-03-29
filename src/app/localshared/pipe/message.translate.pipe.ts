4e7a8fe7c5898e705b158af2ef0b2e9e9d8cf1c7
oopsmails <oopsmails@gmail.com>
2023-03-29 17:29:46 -0400

    temp

--- src/app/localshared/pipe/message.translate.pipe.ts
+++ src/app/localshared/pipe/message.translate.pipe.ts
@@ -5,64 +5,34 @@ import fr from '../../../assets/i18n/fr.json';
 
 @Pipe({ name: 'messageTranslate', pure: false })
 export class MessageTranslatePipe implements PipeTransform {
-  private data = { en, fr };
-  private currentLang = 'fr';
+  constructor(private translate: TranslateService) {}
 
-  constructor(private translateService: TranslateService) {}
-
-  // transform(value: string, args: any): any {
-  //   if (!value) return;
-  //   let translation = this.translateService.instant(value);
-  //   if (args) {
-  //     const keys = Object.keys(args);
-  //     keys.forEach((key) => {
-  //       translation = translation.replace(`{{${key}}}`, args[key]);
-  //     });
-  //   }
-  //   return translation;
-  // }
-
-  transform(key: string, args: any): string {
-    let message = this.getMessage(key);
-    if (!message) {
-      return key;
-    }
-    if (args) {
-      Object.keys(args).forEach((key) => {
-        message = message.replace(`{${key}}`, args[key]);
-      });
-    }
-    return message;
+  transform(key: string, args?: any): string {
+    const en = this.flatten(this.translate.getTranslation('en'));
+    const fr = this.flatten(this.translate.getTranslation('fr'));
+    const value = this.translate.currentLang === 'en' ? en[key] : fr[key];
+    return this.interpolate(value, args);
   }
 
-  /**
-   * This is also working
-   *
-   * @param key
-   * @param context
-   * @returns
-   */
-  transform_also_work(key: string, context?: Record<string, any>): string {
-    let message = this.getMessage(key);
-    if (context) {
-      for (const key in context) {
-        message = message.replace(`{${key}}`, context[key]);
+  interpolate(value: string, args?: any): string {
+    if (!args) {
+      return value;
+    }
+
+    return value.replace(/\{([^}]+)\}/g, (match: string, key: string) => {
+      return args[key] || '';
+    });
+  }
+
+  flatten(obj: any, prefix = ''): any {
+    return Object.keys(obj).reduce((acc, k) => {
+      const pre = prefix.length ? prefix + '.' : '';
+      if (typeof obj[k] === 'object') {
+        Object.assign(acc, this.flatten(obj[k], pre + k));
+      } else {
+        acc[pre + k] = obj[k];
       }
-    }
-    return message;
-  }
-
-  private getMessage(key: string): string {
-    // You can implement your own message lookup logic here
-    // For example, you can use a service to load messages from a file
-    // or a backend API
-    // const messages = {
-    //   greeting: 'Hello {name}!',
-    //   farewell: 'Goodbye {name}!',
-    // };
-    // return messages[key] || key;
-
-    // const lang = 'en'; // get the current language from a service or some other means
-    return this.data[this.currentLang][key] || key;
+      return acc;
+    }, {});
   }
 }
