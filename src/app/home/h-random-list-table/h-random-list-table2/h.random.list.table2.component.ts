847d86d001a6e77c5344473e9723ae139b3b6bd8
oopsmails <oopsmails@gmail.com>
2023-05-25 08:58:58 -0400

    home move and merge

--- src/app/home/h-random-list-table/h-random-list-table2/h.random.list.table2.component.ts
+++ src/app/home/h-random-list-table/h-random-list-table2/h.random.list.table2.component.ts
@@ -1,17 +1,16 @@
+import { ColorEnum, DirectionEnumSimple, getColorEnumName } from '../../../localshared/models/shared-model';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { RandomItem, SharedDataService } from 'oops-lib002';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { RandomItem, SharedDataService, Car } from 'oops-lib002';
 import { Observable, Subject } from 'rxjs';
 import { DirectionEnum } from 'src/app/localshared/models/shared-model';
+import { OopsPaginationService } from 'src/app/localshared/services/oops.pagination.service';
+import { TranslateService } from '@ngx-translate/core';
 import { I18nService } from 'src/app/localshared/services/i18n.service';
-import { ColorEnum, DirectionEnumSimple, getColorEnumName } from '../../localshared/models/shared-model';
 
 interface RandomItemExt extends RandomItem {
   type?: string;
-  displayTypeSelect?: boolean;
-  descErrorMessage?: string;
-  disableDesc?: boolean;
-  disableCustomKey?: boolean;
+  displayType?: boolean;
 }
 
 export enum MyColorEnum {
@@ -22,10 +21,12 @@ export enum MyColorEnum {
 
 @Component({
   selector: 'app-random-list-table2',
-  templateUrl: './h.random.list.table4.component.html',
-  styleUrls: ['./h.random.list.table4.component.scss'],
+  templateUrl: './h.random.list.table2.component.html',
+  styleUrls: ['./h.random.list.table2.component.scss'],
 })
-export class HomeRandomListTable4Component implements OnInit, OnDestroy {
+export class HomeRandomListTable2Component implements OnInit, OnDestroy {
+  private COMPONENT_NAME = 'HomeRandomListTable2Component';
+
   private onDestroy$: Subject<boolean> = new Subject();
 
   items$: Observable<RandomItem[]>;
@@ -41,7 +42,7 @@ export class HomeRandomListTable4Component implements OnInit, OnDestroy {
     { value: DirectionEnumSimple[DirectionEnumSimple.UP], displayKey: 'DIRECTION.UP' },
     { value: DirectionEnumSimple[DirectionEnumSimple.DOWN], displayKey: 'DIRECTION.DOWN' },
     { value: DirectionEnumSimple[DirectionEnumSimple.LEFT], displayKey: 'DIRECTION.LEFT' },
-    { value: DirectionEnumSimple[DirectionEnumSimple.RIGHT], displayKey: 'DIRECTION.RIGHT' },
+    { value: DirectionEnumSimple[DirectionEnum.RIGHT], displayKey: 'DIRECTION.RIGHT' },
   ];
 
   colorSelected: string;
@@ -60,17 +61,54 @@ export class HomeRandomListTable4Component implements OnInit, OnDestroy {
 
   ngOnInit() {
     const colorValue: string = 'Red';
+    const colorEnum: ColorEnum = ColorEnum[colorValue];
 
     this.colorSelected = '';
 
+    this.translate.setDefaultLang('fr');
+    this.translate.use('fr');
+    this.translate.currentLang = 'fr'; // this is used in I18nService
+
+    console.log(
+      this.COMPONENT_NAME + ', ngOnInit, test translateService-1: ',
+      this.i18nService.getJsonValueI18n('COLOR.colors.RED')
+    );
+
+    console.log(
+      this.COMPONENT_NAME + ', ngOnInit, test translateService-2: ',
+      this.i18nService.getJsonValueI18n('TEST_DESCRIPTION')
+    );
+
+    console.log(
+      this.COMPONENT_NAME + ', ngOnInit, Enum to string, only simple: DirectionEnumSimple[DirectionEnumSimple.DOWN] = ',
+      DirectionEnumSimple[DirectionEnumSimple.DOWN]
+    );
+
+    console.log(
+      this.COMPONENT_NAME + ', ngOnInit, Enum to string: String(DirectionEnumSimple.UP) = ',
+      String(DirectionEnumSimple.UP)
+    );
+
     const myColor = MyColorEnum.RED;
+
+    console.log(this.COMPONENT_NAME + ', ngOnInit, Enum to string: myColor.toString() = ', myColor.toString());
+
+    console.log(
+      this.COMPONENT_NAME + ', ngOnInit, Enum to string, only simple: MyColorEnum[MyColorEnum.RED] = ',
+      MyColorEnum[MyColorEnum.RED]
+    );
+
+    console.log(
+      this.COMPONENT_NAME + ', ngOnInit, Enum to string: getColorEnumName(ColorEnum.GREEN) = ',
+      getColorEnumName(ColorEnum.GREEN)
+    );
 
     this.initializeColorTranslations();
 
     this.items$ = this.sharedDataService.getRandomItems(30, 500);
 
     this.directions.push('Select ...');
-    this.addNewRow(5);
+    this.addNewRow();
   }
 
   getDirectionEnumValues() {
@@ -83,51 +121,39 @@ export class HomeRandomListTable4Component implements OnInit, OnDestroy {
   }
 
   onRowClick(item) {
-    console.log('onRowClick, item = ', item);
+    console.log(this.COMPONENT_NAME + ', onRowClick, item = ', item);
   }
 
   onEditableRowClick(item) {
-    console.log('onEditableRowClick, item = ', item);
+    console.log(this.COMPONENT_NAME + ', onEditableRowClick, item = ', item);
   }
 
   onSelectColor(event) {
-    console.log('onSelectColor, event = ', event);
+    console.log(this.COMPONENT_NAME + ', onSelectColor, event = ', event);
     this.colorSelected = event;
   }
 
-  addNewRow(numOfRows: number) {
-    for (let i = 0; i < numOfRows; i++) {
-      this.editItems.push({
-        id: null,
-        name: '',
-        desc: '',
-        customKey: '',
-        type: '',
-        displayTypeSelect: true,
-        descErrorMessage: '',
-      });
-    }
-
+  addNewRow() {
+    this.editItems.push({
+      id: null,
+      name: '',
+      desc: '',
+      customKey: '',
+      type: '',
+      displayType: true,
+    });
     this.removeShouldBeDisabled = this.editItems.length <= 1;
   }
 
   onCustomKeySelected(item, idx) {
-    console.log('onSelectSymbolClick ...........................', item, idx);
+    console.log(this.COMPONENT_NAME + ', onCustomKeySelected, item = ', item);
 
     if (typeof item === 'string') {
-      console.log('1: from child inputOnBlur, item = ', item);
       this.editItems[idx].customKey = item;
       return;
     }
 
-    if (item && item.eventSource && item.eventSource === '[inputOnTabKeydown]') {
-      console.log('2: from child inputOnTabKeydown, item = ', item);
-      this.editItems[idx].customKey = item.eventData;
-      return;
-    }
-
     if (item && item.name) {
-      console.log('3: from child onSelectSymbolClick, item = ', item);
       // this should be the same as receiveSelectItem() in HomeRandomListSearchComponent!!! in face, need only one!!!
       this.editItems[idx].customKey = 'selected-' + item.name;
     } else if (item && item.model) {
@@ -164,50 +190,12 @@ export class HomeRandomListTable4Component implements OnInit, OnDestroy {
     this.removeShouldBeDisabled = this.editItems.length <= 1;
   }
 
-  changeInputItem() {
-    console.log('testing changeInputItem ...');
-    this.editItems[0].customKey = new Date().toLocaleTimeString();
-  }
-
   onTypeChange(event, idx) {
     this.editItems[idx].type = event;
-    // if (event !== '') {
-    //   this.editItems[idx].displayTypeSelect = false;
-    // } else {
-    //   this.editItems[idx].displayTypeSelect = true;
-    // }
-
-    if (event === 'RIGHT') {
-      this.editItems[idx].disableDesc = true;
-      this.editItems[idx].disableCustomKey = true;
-      this.editItems[idx].desc = 'From Right';
-      this.editItems[idx].customKey = 'From Right';
-      this.editItems[idx].displayTypeSelect = false;
+    if (event || '' !== '') {
+      this.editItems[idx].displayType = false;
     } else {
-      if (event !== '') {
-        this.editItems[idx].disableDesc = false;
-        this.editItems[idx].disableCustomKey = false;
-        this.editItems[idx].displayTypeSelect = false;
-        this.editItems[idx].desc = '';
-        this.editItems[idx].customKey = '';
-      } else {
-        this.editItems[idx].displayTypeSelect = true;
-      }
-    }
-  }
-
-  onDescChange(event, idx) {
-    if (this.editItems[idx].desc === '') {
-      this.editItems[idx].descErrorMessage = '';
-      return;
-    }
-    // const allowed_pattern = new RegExp('^[0-9]{1,14}(\\.[0-9]{1,3})?$');
-    const allowed_pattern = new RegExp('^[0-9]{1,4}?$');
-    if (!allowed_pattern.test(this.editItems[idx].desc)) {
-      this.editItems[idx].descErrorMessage = 'Please verify your input ...';
-    } else {
-      this.editItems[idx].descErrorMessage = '';
-      // this.editItems[idx].desc = event.substring(0, event.length - 1); // should NOT do this
+      this.editItems[idx].displayType = true;
     }
   }
 
