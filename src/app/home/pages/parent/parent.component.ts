83bd26ac5859b77a76fa4dd3a34ccb9ae75d4bc2
oopsmails <oopsmails@gmail.com>
2023-07-27 10:30:28 -0400

    subject in sharedservice, from parent to child and refresh

--- src/app/home/pages/parent/parent.component.ts
+++ src/app/home/pages/parent/parent.component.ts
@@ -1,5 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgxSelectComponent } from 'ngx-select-ex';
+import { Subject, takeUntil } from 'rxjs';
+import { UserDataService } from 'src/app/localshared/services/user.data.service';
 
 @Component({
   selector: 'app-parent',
@@ -9,14 +11,22 @@ import { NgxSelectComponent } from 'ngx-select-ex';
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss'],
 })
-export class ParentComponent {
+export class ParentComponent implements OnInit {
   private compmentName: string = 'ParentComponent';
+  private onDestroy$: Subject<boolean> = new Subject();
+
   message = 'Hello from Parent Component!';
   inputObject: any = { prop1: 'value1', prop2: new Date().toLocaleTimeString() };
 
   manualSearch: string;
 
   @ViewChild('selectComp', { static: false }) selectComp: NgxSelectComponent;
+
+  selectedLanguage: 'EN' | 'FR' = 'EN';
+
+  constructor(private userDataService: UserDataService) {}
+
+  ngOnInit() {}
 
   receivedMessage(event) {
     console.log(this.compmentName, 'receivedMessage, event =', event);
@@ -27,5 +37,10 @@ export class ParentComponent {
 
   changeInputObject() {
     this.inputObject = { prop1: 'new value1', prop2: new Date().toLocaleTimeString() };
+  }
+
+  onSelectedLanguageChange(event: string) {
+    console.log('onSelectedLanguageChange, event = ', event);
+    this.userDataService.currentLocale$.next(event);
   }
 }
